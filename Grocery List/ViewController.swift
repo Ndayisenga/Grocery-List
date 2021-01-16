a7a13c3e4b6a4f08954714c1bc3aaac38c8d1d0e
John <cloude056@yahoo.fr>
2021-01-16 12:35:17 +0200

    Initial Commit

---
+++ Grocery List/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Grocery List
+//
+//  Created by Jean Claude Ndayisenga on 16/01/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
