71128cd5ff80928090048ef688fa2d4e3f257e66
John <cloude056@yahoo.fr>
2021-01-17 12:16:37 +0200

    My  First  Apple Modified

--- Grocery List Watch Extension/InterfaceController.swift
+++ Grocery List Watch Extension/InterfaceController.swift
@@ -10,25 +10,38 @@ import Foundation
 
 
 class InterfaceController: WKInterfaceController {
-
-    @IBOutlet var myButton: WKInterfaceButton!
+    
+    @IBOutlet var myTable: WKInterfaceTable!
     
     override func awake(withContext context: Any?) {
         // Configure interface objects here.
+        super.awake(withContext: context)
+        
+        let array = ["apples", "oranges", "grapes", "banana", "avocado"]
+        
+        myTable.setNumberOfRows(5, withRowType: "cell")
+        
+        var x = 0
+        for fruit in array {
+            
+            let row = myTable.rowController(at: x) as! RowController
+            row.myLabel.setText(fruit)
+            x += 1
+        }
     }
     
     override func willActivate() {
         // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
     }
     
     override func didDeactivate() {
         // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
     }
-
     
-    @IBAction func buttonTapped()
-    {
-        myButton.setTitle("Tapped!")
-    }
+    
+    
+    
     
 }
---
+++ Grocery List Watch Extension/RowController.swift
@@ -0,0 +1,16 @@
+//
+//  RowController.swift
+//  Grocery List Watch Extension
+//
+//  Created by Jean Claude Ndayisenga on 17/01/2021.
+//
+
+import UIKit
+
+import WatchKit
+
+class RowController: NSObject {
+    @IBOutlet var myLabel:WKInterfaceLabel!
+    
+
+}
